c7fc1c56ff82841dca4bb609a15cb554f44bb857
Eric Burns <eburns000@byui.edu>
2022-11-05 10:19:47 -0700

    updates to swagger to allow for authorization

--- swagger.js
+++ swagger.js
@@ -1,16 +1,31 @@
-const swaggerAutogen = require('swagger-autogen')();
+const swaggerAutogen = require("swagger-autogen")();
 
 const doc = {
   info: {
-    title: 'Home Expercise Program API',
-    description: 'This Home Exercise Program API is part of CSE 341 Project 2, Weeks 5-8',
+    title: "Home Expercise Program API",
+    description:
+      "This Home Exercise Program API is part of CSE 341 Project 2, Weeks 5-8",
   },
-  host: '',
-  schemes: ['https','http'],
+  host: "",
+  schemes: ["https", "http"],
+  securityDefinitions: {
+    Authorization: {
+      type: "apiKey",
+      name: "Authorization",
+      in: "header",
+      description: "Authentication token (Bearer)",
+      example: "Bearer <your token>",
+    },
+  },
+  security: [
+    {
+      Authorization: [],
+    },
+  ],
 };
 
-const outputFile = './swagger.json';
-const endpointsFiles = ['./routes/index.js'];
+const outputFile = "./swagger.json";
+const endpointsFiles = ["./routes/index.js"];
 
 /* NOTE: if you use the express Router, you must pass in the 
    'endpointsFiles' only the root file where the route starts,
